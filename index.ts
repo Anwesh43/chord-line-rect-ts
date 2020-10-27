5d4044c661216a39ce0dd575420bbc02ea556212
Anwesh43 <anweshthecool0@gmail.com>
2020-10-27 17:33:07 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,46 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.stroke()
+    }
+
+    static drawClippedRect(context : CanvasRenderingContext2D, r : number, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const x : number = -r * Math.cos(deg)
+        const y : number = r * Math.sin(deg)
+        const x1 : number = r * Math.cos(deg)
+        context.save()
+        context.beginPath()
+        context.arc(x, y, r * sf1, 0, 2 * Math.PI)
+        context.stroke()
+        context.clip()
+        DrawingUtil.drawLine(context, x, y, x + (x1 - x) * sf2, y)
+        context.fillRect(x, y, (x1 - x) * sf3, r - y)
+        context.restore()
+    }
+
+    static drawCLRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const r : number = Math.min(w, h) / sizeFactor 
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawClippedRect(context, r, scale) 
+    }
+}
